716571d67c25b58dfbcb5472c49c1fa14c7e0697
Mark Gowen <mark.gowen78@gmail.com>
2019-09-20 08:33:41 -0700

    installed dependencies, created CharacterCard and CharacterList

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,21 @@
 import React from "react";
+import styled from 'styled-components'
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Card = styled.div``;
+
+const CardName = styled.h1``;
+
+const CardText = styled.p``;
+
+const  CharacterCard = props => {
+  return (
+    <Card key={props.id}>
+      <CardName>{props.name}</CardName>
+      <CardText>Status: {props.status}</CardText>
+      <CardText>Species: {props.species}</CardText>
+      <CardText>Gender: {props.gender}</CardText>
+    </Card>
+    );
 }
+
+export default CharacterCard;
