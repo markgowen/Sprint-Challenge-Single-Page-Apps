0b5544a2a1b94653f54c9709fa9630b84ad3f5be
Mark Gowen <mark.gowen78@gmail.com>
2019-09-20 10:21:41 -0700

    added styled-components

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,24 +1,43 @@
-import React from "react";
-import styled from 'styled-components'
+import React from 'react';
+import styled from 'styled-components';
 
-const Card = styled.div``;
+const Card = styled.div`
+  width: 50%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  margin: auto;
+  background: #0f0b0d;
+  border-radius: 10px;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  margin: 2% auto;
+  padding: 1%;
+`;
 
-const CardImage = styled.img``;
+const CardImage = styled.img`
+  margin: 30px auto;
+`;
 
-const CardName = styled.h1``;
+const CardName = styled.h1`
+  color: #2c8bcf;
+`;
 
-const CardText = styled.p``;
+const CardText = styled.p`
+  color: #ab856e;
+  margin-bottom: 2%;
+`;
 
-const  CharacterCard = props => {
+const CharacterCard = props => {
   return (
     <Card key={props.id}>
-      <CardImage alt='Rick and Morti Character' src ={props.image}></CardImage>
+      <CardImage alt="Rick and Morti Character" src={props.image}></CardImage>
       <CardName>{props.name}</CardName>
       <CardText>Status: {props.status}</CardText>
       <CardText>Species: {props.species}</CardText>
       <CardText>Gender: {props.gender}</CardText>
     </Card>
-    );
-}
+  );
+};
 
 export default CharacterCard;
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,10 +1,37 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import axios from 'axios'
+import CharacterCard from './CharacterCard'
 
 export default function SearchForm() {
- 
-  return (
-    <section className="search-form">
-     // Add a search form here
-    </section>
-  );
+  // const [charName, setCharName] = useState([]);
+
+  // useEffect(() => {
+  //   // TODO: Add API Request here - must run in `useEffect`
+  //   //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
+  //   axios
+  //     .get('https://rickandmortyapi.com/api/character/')
+  //     .then(res => {
+  //       const charName = res.data.results;
+  //       console.log('Rick & Morti Characters', charName)
+  //       setCharName(charName);
+  //       console.log(charName)
+  //     })
+  //     .catch(error => {
+  //       console.log('No characters returned', error);
+  //     })
+  // }, []);
+
+  // return (
+  //   <div className='Container'>
+  //     <div className='CardRow'>
+  //       {charName.filter(list => {
+  //         return (
+  //           <CharacterCard key={list.id}
+  //           name={list.name}
+  //           />
+  //         )
+  //     })}
+  //     </div>
+  //   </div>
+  // );
 }
